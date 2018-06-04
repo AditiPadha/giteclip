20a6f60737db1a89085d356894843bd27f7c4252
AditiPadha <yesaditi@gmail.com>
2018-06-04 16:13:19 +0530

    my first commit

---
+++ src/test/java/JenkinsDemo/JenkinsIntegrationTest.java
@@ -0,0 +1,15 @@
+package JenkinsDemo;
+
+//import org.testng.annotations.Test;
+
+import org.junit.Test;
+
+public class JenkinsIntegrationTest {
+	
+	@Test
+	public void testJenkins()
+	{
+		System.out.println("Jenkins execution Test ");
+	}
+
+}
